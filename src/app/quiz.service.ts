925d98ddf525e8a76805508e7188fb9ff2fca982
udarc <46758935+udarc@users.noreply.github.com>
2019-04-08 18:56:08 -0500

    Createdd quiz Services
    ng g service quiz

---
+++ src/app/quiz.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor() { }
+
+  getQuizzes () {
+    return [
+      {name: 'Quiz 1', numberOfQuestion: 3},
+    
+      ,{name: 'Quiz 2', numberOfQuestion: 0}
+    ]
+  }
+}
