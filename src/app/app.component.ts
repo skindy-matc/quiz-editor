c2e5d017d8c2bf64c36def61bd06c7c3349e6515
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-04-08 18:05:57 -0500

    demo'd attribute binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+  myWidth = 250;
+  makeImageLarger = () => {
+    this.myWidth *= 1.3;
+  }
+  // read-only/getter property
+  get titleColor() {
+    // if my width exceeds 400px set title color to red
+    // else set to bluey
+    return this.myWidth > 400 ? 'red' : 'blue';
+  }
 }
