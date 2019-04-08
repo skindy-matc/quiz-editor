cd30483e303270a439eff6b0c51512713c76b91a
udarc <46758935+udarc@users.noreply.github.com>
2019-04-08 18:06:34 -0500

    Added Event property and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+  myWidth = 250;
+  makeImageLarger() {
+    this.myWidth *= 1.3;
+
+    //ReadOnly/getter property
+
+    
+  }
+  get titleColor () {
+    return this.myWidth >400 ? "red": "blue";
+  }
 }
