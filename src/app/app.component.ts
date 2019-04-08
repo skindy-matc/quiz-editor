58015e8fa52340bff5c97922aa1d2ab4271036de
TanwiSharma <tsharma@madisoncollege.edu>
2019-04-08 18:05:17 -0500

    Demo style and attribute binding
    Demo style and attribute binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+  myWidth = 250;
+
+  makeImageLarger(){
+    this.myWidth *=1.3;
+  }
+  //Read only #get Property
+  get titleColor() {
+    return this.myWidth > 400 ? "red" : "blue";
+  }
 }
