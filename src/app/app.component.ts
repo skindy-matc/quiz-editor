9127b4255d6445c2372a7c85fd0600fd8d2c8725
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-04-08 19:28:13 -0500

    selected quiz behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,25 +19,14 @@ export class AppComponent implements OnInit {
   }
 
   quizzes: QuizDisplay[] = [];
+  selectedQuiz: QuizDisplay;
+
+  selectQuiz(q: QuizDisplay) {
+    this.selectedQuiz = q;
+  }
 
   // so use ngOnInit instead
   ngOnInit() {
     this.quizzes = this.qSvc.getQuizzes();
-  }
-
-  title = 'quiz-editor';
-  myWidth = 250;
-  makeImageLarger = () => {
-    this.myWidth *= 1.3;
-  }
-  // read-only/getter property
-  get titleColor() {
-    // if my width exceeds 400px set title color to red
-    // else set to bluey
-    return this.myWidth > 400 ? 'red' : 'blue';
-  }
-
-  get myWidthBool() {
-    return this.myWidth > 400;
   }
 }
