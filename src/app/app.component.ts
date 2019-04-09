49cd8df944b97251d194f9df0f2ec5fa786f61e1
dheiman1 <dylanheiman@gmail.com>
2019-04-08 19:17:41 -0500

    Accessability and stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component , OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  name: string;
+  numberOfQuestions: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,8 +20,11 @@ export class AppComponent implements OnInit {
      */ 
   }
 
+  quizzes: QuizDisplay[] = [];
+
   ngOnInit() {
-    console.log(this.qSvc.getQuizzes());
+    // console.log(this.qSvc.getQuizzes());
+    this.quizzes = this.qSvc.getQuizzes();
   }
 
   title = 'quiz-editor';
