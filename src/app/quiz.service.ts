c85f2e4c9062afecc9fc0a67c7367ff43b967247
dheiman1 <dylanheiman@gmail.com>
2019-04-08 20:35:12 -0500

    Calling a REST endpoint

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -9,10 +9,13 @@ export class QuizService {
   constructor(private builtInAngularHttpClient: HttpClient) { }
 
   getQuizzes() {
+
+    return this.builtInAngularHttpClient.get("https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz");
+    
     // Dummy array of quiz
-    return [
-      { name: 'Quiz 1', numberQuestions: 3 }
-      , { name: 'Quiz 2', numberQuestions: 0 }
-    ];
+    // return [
+    //   { name: 'Quiz 1', numberQuestions: 3 }
+    //   , { name: 'Quiz 2', numberQuestions: 0 }
+    // ];
   }
 }
