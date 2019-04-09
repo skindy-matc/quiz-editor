f43b7f68ead30e11a0f217635dc62294286276d9
TanwiSharma <tsharma@madisoncollege.edu>
2019-04-08 20:21:41 -0500

    Take dependency on HttpClient
    Dont forget to import HttpClientModule.

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
   getQuizes() {
     return [
       {name: 'quiz 1', numberQuestion: 3}
