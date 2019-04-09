7cfa1f2832e55c81a42e62177a0143250f8cc3ed
miller-ryan <35612874+miller-ryan@users.noreply.github.com>
2019-04-08 19:13:48 -0500

    Fake quizzes showing in UL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  name: string;
+  numberOfQuestions: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,12 +16,13 @@ export class AppComponent implements OnInit {
   constructor(private qSvc: QuizService) {
     // Use the quiz service here, but if it fails the creation 
     // of the component fails :-(
-
-
   }
 
+  quizzes: QuizDisplay[] = [];
+
   ngOnInit() {
-    console.log(this.qSvc.getQuizzes());
+    //console.log(this.qSvc.getQuizzes());
+    this.quizzes = this.qSvc.getQuizzes();
   };
 
   title = 'quiz-editor';
