8ca1fc2642c4aad68706edb2ccb7273c2866ac36
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-04-08 20:21:41 -0500

    DI for built-in angular http client - have to also update module

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // have to manually update from str
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private angularHttpClient: HttpClient) { }
 
   getQuizzes() {
     // dummy array of quiz objects
